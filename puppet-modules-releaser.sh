a9a713fbeb398790c03ae97ee79a3167394dd2bb
Paul Belanger <paul.belanger@polybeacon.com>
2012-06-24 17:16:21 -0400

    Lets build and upload the tarball too Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- puppet-modules-releaser.sh
+++ puppet-modules-releaser.sh
@@ -11,6 +11,7 @@ MAJOR="$(echo ${VERSION} | cut -d '.' -f 1)"
 MINOR="$(echo ${VERSION} | cut -d '.' -f 2)"
 MAINTENANCE="$(echo ${VERSION} | cut -d '.' -f 3)"
 VERSIONBUMP="${MAJOR}.${MINOR}.$((${MAINTENANCE} + 1))"
+TARBALL=${PROJECT}-${VERSION}.tar.gz
 
 git tag -u 6E14C2BE v${VERSION} -m "Release v${VERSION}"
 
@@ -19,3 +20,9 @@ sed -i "s/'${VERSION}'/'${VERSIONBUMP}'/g" Modulefile
 git commit -a -s -m "Bump module version to ${VERSIONBUMP}"
 git push origin master
 git push origin v${VERSION}
+
+git checkout v${VERSION}
+puppet-module build
+
+cd pkg
+~/workspace/kickstand-ci/publish-tarball.py $TARBALL --user kickstandproject --repo $PROJECT
